2081fab5907d618cee62ce275fb20e90182c086f
tondotiz <tondotiz@gmail.com>
2018-11-05 17:36:00 -0500

    Update NTP_7_segment_clock.ino

--- NTP_7_segment_clock.ino
+++ NTP_7_segment_clock.ino
@@ -1,85 +1,61 @@
+#include <Adafruit_NeoPixel.h>
+
 // it did not work with captive portals or at least I was unable to fixe it for captive portal
 //I have fully functional and running if you have any problems with please check the pointer
 // if someone knows how to fix it please tell me as well.
-
-
- //original idea credited to *****Ivan Miranda*****
-
-
-
+//original idea credited to *****Ivan Miranda*****
 // 
-
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <Adafruit_NeoPixel.h>
 #include <cstdlib> // for std::srand()
 #include <algorithm> // for std::random_shuffle()
-
 char ssid[] = "----------------------------";
 char pass[] = "---------------------------";
-                                            
 int hours =0;
 int mins =0;
-int C =0 ;  /*
-int R = 0; 
-int G = 0;
-int B = 0; */
+int C ;
 int NN = 0;
-
-
-
+// NeoPixels Numbers
 #define NUM_PIXELS 100
+// NeoPixels output pin you can chenge this based on your needs
 #define PIN_PIXELS 14
-
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, PIN_PIXELS, NEO_GRB + NEO_KHZ800);
-
 unsigned int localPort = 123;
-
 IPAddress timeServerIP;
 const char* ntpServerName = "time.chu.nrc.ca";
-
 const int NTP_PACKET_SIZE = 48;
-
 byte packetBuffer[ NTP_PACKET_SIZE];
-
 WiFiUDP udp;
+int RGBPIXEL(int);
 
 void setup()
 {
  Serial.begin(115200);
  Serial.println();
-
  Serial.print("Connecting to ");
  Serial.println(ssid);
  WiFi.begin(ssid, pass);
-
  while (WiFi.status() != WL_CONNECTED) {
-   delay(500);
-   Serial.print(".");
- }
+     delay(500);
+     Serial.print(".");}
  Serial.println("");
-
  Serial.println("WiFi connected");
  Serial.println("IP address: ");
  Serial.println(WiFi.localIP());
-
  Serial.println("Starting UDP");
  udp.begin(localPort);
  Serial.print("Local port: ");
  Serial.println(udp.localPort());
  pixels.begin();
  RGBPIXEL(C);
-
 }
 
 void loop()
 {
  WiFi.hostByName(ntpServerName, timeServerIP); 
-
  sendNTPpacket(timeServerIP); 
  delay(1000);
-
-
  int cb = udp.parsePacket();
  if (!cb) {
    Serial.println("no packet yet");
@@ -87,33 +63,35 @@ void loop()
  else {
    Serial.print("packet received, length = ");
    Serial.println(cb);
-
    udp.read(packetBuffer, NTP_PACKET_SIZE);
-
    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
    unsigned long secsSince1900 = highWord << 16 | lowWord;
    Serial.print("Seconds since Jan 1 1900 = " );
    Serial.println(secsSince1900);
-
    // now convert NTP time into everyday time:
    Serial.print("Unix time = ");
    const unsigned long seventyYears = 2208988800UL;
    unsigned long epoch = secsSince1900 - seventyYears;
-   //Serial.print("The EPOCH is ");
+   Serial.print("The Unix Time is: ");
    Serial.println(epoch);
    // the time diffrence from UTC to UTC-5:  
-   // print the hour, minute and second:
    Serial.print("The UTC time is: "); 
    // print Unix time:
-   //Serial.print("The EPOCH is ");
+   Serial.print("The Day is ");
+   Serial.print(((epoch  % 86400L)));
+  Serial.print("The Month is ");
+   Serial.print(((epoch  % 2629743L)));
+   Serial.print("The Year is ");
+   Serial.print(((epoch  % 31556926L)));
+   
+   // print the hour, minute and second:
    Serial.print(((epoch  % 86400L) / 3600));
    Serial.print(":");
    Serial.println((epoch  % 3600) / 60);
    Serial.print("Toronto Time: ");
    //Toronto Time:
    epoch = epoch - 18000;
-
    hours = ((epoch  % 86400L) / 3600);
    if (hours<24)
    {
@@ -124,57 +102,13 @@ void loop()
      hours=0;  
    }
    mins = (epoch  % 3600) / 60;
-
    Serial.print(hours); 
    Serial.print(':');
    Serial.println(mins); 
-
-
    //random RGB genrator
    //Let's make it more random
-   /*
-   int R = random(0,256); 
-   int G = random(0,256);
-   int B = random(0,256);
-   Serial.println("RGB :");
-   Serial.print("R  =   ");
-   Serial.println(R);
-   Serial.print("G  =   ");
-   Serial.println(G);
-   Serial.print("B  =   ");
-   Serial.println(B);
-   RGBPIXEL(R),RGBPIXEL(G),RGBPIXEL(B)
-   drawdigit(0,R,G,B,hours/10);//Draw the first digit of the hour
-   drawdigit(21,R,G,B,hours-((hours/10)*10));//Draw the second digit of the hour
-   pixels.setPixelColor(42, pixels.Color(R,G,B));//Draw the two middle dots
-   pixels.setPixelColor(43, pixels.Color(R,G,B));
-   drawdigit(44,R,G,B,mins/10);//Draw the first digit of the minute
-   drawdigit(65,R,G,B,mins-((mins/10)*10));//Draw the second digit of the minute
-   */
-
-   /*
-   drawdigit(0,R,G,B,hours/10);//Draw the first digit of the hour
-   drawdigit(21,R,G,B,hours-((hours/10)*10));//Draw the second digit of the hour
-   pixels.setPixelColor(42, pixels.Color(R,G,B));//Draw the two middle dots
-   pixels.setPixelColor(43, pixels.Color(R,G,B));
-   drawdigit(44,R,G,B,mins/10);//Draw the first digit of the minute
-   drawdigit(65,R,G,B,mins-((mins/10)*10));//Draw the second digit of the minute
-*/
-
- /*
-
-   drawdigit(0,RGBPIXEL(R),RGBPIXEL(G),RGBPIXEL(B),hours/10);//Draw the first digit of the hour
-   Serial.print("G  =   ");
-   Serial.println(G);
-   drawdigit(21,RGBPIXEL(R),RGBPIXEL(G),RGBPIXEL(B),hours-((hours/10)*10));//Draw the second digit of the hour
-   pixels.setPixelColor(42, pixels.Color(RGBPIXEL(R),RGBPIXEL(G),RGBPIXEL(B)));//Draw the two middle dots
-   pixels.setPixelColor(43, pixels.Color(RGBPIXEL(R),RGBPIXEL(G),RGBPIXEL(B)));
-   drawdigit(44,RGBPIXEL(R),RGBPIXEL(G),RGBPIXEL(B),mins/10);//Draw the first digit of the minute
-   drawdigit(65,RGBPIXEL(R),RGBPIXEL(G),RGBPIXEL(B),mins-((mins/10)*10));//Draw the second digit of the minute
-   */
-
    C = random(0,256);
-       Serial.print("C1  =   ");
+   Serial.print("C1  =   ");
    Serial.println(C);
    drawdigit(0,RGBPIXEL(C),RGBPIXEL(C),RGBPIXEL(C),hours/10);//Draw the first digit of the hour
    Serial.print("CCCC  =   ");
@@ -184,22 +118,11 @@ void loop()
    pixels.setPixelColor(43, pixels.Color(RGBPIXEL(C),RGBPIXEL(C),RGBPIXEL(C)));
    drawdigit(44,RGBPIXEL(C),RGBPIXEL(C),RGBPIXEL(C),mins/10);//Draw the first digit of the minute
    drawdigit(65,RGBPIXEL(C),RGBPIXEL(C),RGBPIXEL(C),mins-((mins/10)*10));//Draw the second digit of the minute
-
-
-
-
-
    delay(59500);//Request to NIST server should be separated by 60 seconds.
-
  }
 }
-
-
-
-
 void drawdigit(int offset, int r, int g, int b, int n)
 {
-
  if (n==2 || n==3 || n== 4 || n==5 || n==6 || n==8 || n==9 )//MIDDLE
  {
    pixels.setPixelColor(0+offset, pixels.Color(r,g,b));
@@ -286,9 +209,6 @@ void drawdigit(int offset, int r, int g, int b, int n)
  }
  pixels.show();
 }
-
-
-
 // send an NTP request to the time server at the given address
 unsigned long sendNTPpacket(IPAddress& address)
 {
@@ -306,15 +226,13 @@ unsigned long sendNTPpacket(IPAddress& address)
  packetBuffer[13]  = 0x4E;
  packetBuffer[14]  = 49;
  packetBuffer[15]  = 52;
-
  // all NTP fields have been given values, now
  // you can send a packet requesting a timestamp:
  udp.beginPacket(address, 123); //NTP requests are to port 123
  udp.write(packetBuffer, NTP_PACKET_SIZE);
  udp.endPacket();
 }
-
-
+//Random color maker
 int RGBPIXEL(int C=0){
  C = random(0,256);
  Serial.print("C  =   ");
@@ -323,20 +241,4 @@ int RGBPIXEL(int C=0){
    Serial.print("NN  =   ");
    Serial.println(NN);
    return C;
- }
-/*
-
-
-
-
-
-Colour  R G B
-Red 255 0 0
-Orange  255 32  0
-Yellow  255 127 0
-Green 0 255 0
-Blue  0 0 255
-Indigo  40  0 255
-Violet  255 0 80
-
-*/
+   }
